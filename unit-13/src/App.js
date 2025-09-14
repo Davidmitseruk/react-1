690ab10aff9993ae2efccb6b29414f72e33291de
Davidmitseruk <davidmitseruk@gmail.com>
2025-09-14 19:43:25 +0200

    ok

---
+++ unit-13/src/App.js
@@ -0,0 +1,68 @@
+import { Component } from "react";
+
+class ToDo extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      tasks: [],
+      newTask: "",
+    }
+}
+componentDidMount(){
+  const saveTasks = localStorage.getItem("tasks")
+  if(saveTasks){
+    this.setState({tasks: JSON.parse(saveTasks)})
+  }
+}
+componentDidUpdate(prevProps, prevState){
+  if(prevState.tasks !== this.state.tasks){
+    localStorage.setItem("tasks", JSON.stringify(this.state.tasks))
+  }
+}
+handleChange = (e) => {
+  this.setState({newTask: e.target.value})
+}
+addTask = () => {
+  const {newTask, tasks} = this.state;
+  if(newTask.trim() !== ""){
+    this.setState({tasks: [...tasks, newTask],
+      newTask: ""
+    })
+  }
+}
+deleteTask = (index) => {
+  const updatedTask = this.state.tasks.filter((_, i) => i !== index);
+  this.setState({tasks: updatedTask})
+}
+render(){
+  return(
+    <div className="toDoContainer">
+      <h1>
+        ToDo List
+      </h1>
+      <div>
+        <input
+        value={this.state.newTask}
+        onChange={this.handleChange}
+        />
+        <button
+        onClick={this.addTask}
+        >Add</button>
+      </div>
+      <ul>
+        {this.state.tasks.map((task, index) => (
+          <li key={index}>
+            <p>{task}</p>
+            <button
+            onClick={() => this.deleteTask(index)}
+            >Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+}
+
+
+export default ToDo;
